b2098d0b9eab39f42134c757045caef93fe15349
Maham Nadeem <109579257+maham1033@users.noreply.github.com>
2024-06-24 18:38:32 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,15 @@ import './index.css';
 
 import { CounterProvider } from './context/Counter.jsx';
 
-// 2nd
-// import {CartProvider} from './context/Cart.jsx';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
     <CounterProvider>
 
-      {/* <CartProvider> */}
+     
       <App />
-      {/* </CartProvider> */}
+   
 
     </CounterProvider>
   </React.StrictMode>
